8245946c4f71d944f24d79f149b0c6faf1152a80
René Léveillé <game2world@yahoo.ca>
2017-11-30 19:48:07 -0500

    added header and basic shortcode

--- src/nomad-map.php
+++ src/nomad-map.php
@@ -1,5 +1,27 @@
 <?php
 /*
 Plugin Name: Nomad Map
+Plugin URI: https://github.com/Progdrasil/nomad-map
+Version: 0.1.0
+Author: Rene Leveille
+Author URI: https://progdrasil.com
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+Text Domain: nomad-map
+Description: An automatic interactive map that places a marker for each geolocated post.
 */
+
+// HOOKS
+add_action('init', 'nm_register_shortcodes');
+
+// SHORTCODES
+function nm_register_shortcodes() {
+	add_shortcode('nomad-map', 'nm_display_map_shortcode');
+}
+
+function nm_display_map_shortcode( $args ) {
+	return '<p> I AM THE AWESOME MAP </p>';
+}
+
+
 ?>
